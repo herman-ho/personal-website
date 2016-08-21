a95409cc04dafa2b0c4a5729f2927c08ab2c10be
Herman Ho <contact.hermanho@gmail.com>
2016-08-21 13:25:40 -0400

    beautify code without changing content

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,39 +1,23 @@
 $(function() {
-
-	$('a[href*="#"]:not([href="#"])').click(function() {
-
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
-			var target = $(this.hash);
-
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-			if (target.length) {
-
-				$('html, body').animate({
-
-					scrollTop: target.offset().top
-
-				}, 500);
-
-				return false;
-
-			}
-
-		}
-
-	});
-
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname
+            .replace(/^\//, '') && location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this
+                .hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
+        }
+    });
 });
-
-$(function(){
-
-		$(".type").typed({
-
-				strings: ["Herman Ho"],
-
-				typeSpeed: 50
-
-		});
-
+$(function() {
+    $(".type").typed({
+        strings: ["Herman Ho"],
+        typeSpeed: 50
+    });
 });
